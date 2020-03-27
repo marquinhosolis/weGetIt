c1d02152d2b48b1a21ef83c55a1a0f51b5ac49f4
marquinhosolis <marquinhosolis@gmail.com>
2020-03-26 23:39:03 -0300

    Cover Section Home

--- src/front-page.php
+++ src/front-page.php
@@ -6,6 +6,16 @@ Template Name: Home Page
 
 <?php get_header(); ?>
 	<main class="homePage">
-
+		<section class="cover coverHome">
+			<div class="container">
+				<div class="coverHomeText">
+					<h1>Premium performance</h1>
+					<h2>without the premium price tag</h2>
+				</div>
+				<div class="coverHomeProducts">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wp/home-cover-products.png" alt="">
+				</div>
+			</div>
+		</section>
 	</main>
 <?php get_footer(); ?>		
