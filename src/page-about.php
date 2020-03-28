49ab816bfb878f98fd3d2eee0b2541c75beb27fe
marquinhosolis <marquinhosolis@gmail.com>
2020-03-28 10:41:23 -0300

    About us page section 2

--- src/page-about.php
+++ src/page-about.php
@@ -20,7 +20,19 @@ Template Name: About Us Page
             <div class="aboutUsIntroText">
                 <h2>Make every day shine<br><span>with WeGetIt</span></h2>
                 <p>Designed to fit your family’s busy day to day life, our mission is to create quality, affordable cleaning products that simply work. <br><br>With Urca, you can check off all your cleaning and laundry tasks quickly. All that’s left is for you to enjoy spending time together with family in your beautifully clean home!
-</p>
+                </p>
+            </div>
+        </div>
+    </section>
+    <section class="aboutUsSection2">
+        <div class="container">
+            <div class="aboutUsSection2Image">
+                <img class="aboutUsIntroImageDrops" src="<?php echo get_stylesheet_directory_uri(); ?>/images/drops-about.png" alt="About us Intro Image">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wp/about-us-section2-image.png" alt="About us Intro Image">
+            </div>
+            <div class="aboutUsSection2Text">
+                <h2>WeGetIt first began in Brazil <span>over 40 years ago, </span></h2>
+                <p>winning the loyalty of families nationwide with our traditional coconut soap. Since then, we’ve expanded to other countries in South America and the United States, and our range of products has grown to include all purpose cleaners, laundry detergents, fabric softeners, and more.</p>
             </div>
         </div>
     </section>
