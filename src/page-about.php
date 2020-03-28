7b21fe2cc4d2f4be471a857464b0f4ec84b6fc99
marquinhosolis <marquinhosolis@gmail.com>
2020-03-28 13:02:16 -0300

    Including animations on about page

--- src/page-about.php
+++ src/page-about.php
@@ -8,16 +8,16 @@ Template Name: About Us Page
 <main class="contactPage">
     <section class="aboutUsCover">
         <div class="container">
-            <h1>About WeGetIt</h1>
+            <h1 data-aos="fade-up">About WeGetIt</h1>
         </div>
     </section>
     <section class="aboutUsIntro">
         <div class="container">
-            <div class="aboutUsIntroImage">
+            <div class="aboutUsIntroImage" data-aos="fade-right">
                 <img class="aboutUsIntroImageDrops" src="<?php echo get_stylesheet_directory_uri(); ?>/images/drops-about.png" alt="About us Intro Image">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wp/about-intro-image.png" alt="About us Intro Image">
             </div>
-            <div class="aboutUsIntroText">
+            <div class="aboutUsIntroText" data-aos="fade-left">
                 <h2>Make every day shine<br><span>with WeGetIt</span></h2>
                 <p>Designed to fit your family’s busy day to day life, our mission is to create quality, affordable cleaning products that simply work. <br><br>With Urca, you can check off all your cleaning and laundry tasks quickly. All that’s left is for you to enjoy spending time together with family in your beautifully clean home!
                 </p>
@@ -26,22 +26,22 @@ Template Name: About Us Page
     </section>
     <section class="aboutUsSection2">
         <div class="container">
-            <div class="aboutUsSection2Image">
+            <div class="aboutUsSection2Image" data-aos="fade-left">
                 <img class="aboutUsIntroImageDrops" src="<?php echo get_stylesheet_directory_uri(); ?>/images/drops-about.png" alt="About us Intro Image">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wp/about-us-section2-image.png" alt="About us Intro Image">
             </div>
-            <div class="aboutUsSection2Text">
+            <div class="aboutUsSection2Text" data-aos="fade-right">
                 <h2>WeGetIt first began in Brazil <span>over 40 years ago, </span></h2>
                 <p>winning the loyalty of families nationwide with our traditional coconut soap. Since then, we’ve expanded to other countries in South America and the United States, and our range of products has grown to include all purpose cleaners, laundry detergents, fabric softeners, and more.</p>
             </div>
         </div>
     </section>
     <section class="aboutUsDrops">
-        <div class="container">
+        <div class="container" data-aos="fade-up">
             <div class="flexslider dropsSlider">
                 <ul class="slides">
-                    <li>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/purple-drop.png" alt="drop">
+                    <li >
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/purple-drop.png" alt="drop" >
                         <h3>Trusted by families for over 40 years</h3>
                     </li>
                     <li>
