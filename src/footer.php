728f7cbe1cc1eef2e6cb27ebc7459aa3881a5673
marquinhosolis <marquinhosolis@gmail.com>
2020-03-27 22:01:25 -0300

    Footer

--- src/footer.php
+++ src/footer.php
@@ -1,7 +1,31 @@
 <footer>
-	<?php require_once('partials/logo-dppa-footer.php'); ?>
+	<div class="container">
+		<div class="footerLogo">
+			<a href="<?php echo site_url(); ?>">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/header-logo.png" alt="We Get It Logo">
+			</a>
+		</div>
+		<div class="footerAddress">
+			<address>
+				<a href="https://www.google.com.br/maps/search/123+Address+Street+Orlando,+Fl+32801/@28.544412,-81.3803989,17z/data=!3m1!4b1">
+					123 Address Street <br> Orlando, Fl 32801
+				</a>
+			</address>
+			<div class="socialFooter">
+				<a href="#facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+				<a href="#instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+			</div>
+		</div>
+		<div class="footerNewsletter">
+			waiting for wordpress to implement
+		</div>
+		<div class="footerDppaLogo">
+			<?php require_once('partials/logo-dppa-footer.php'); ?>
+		</div>
+	</div>
 </footer>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script src="https://use.fontawesome.com/37d8e8fa0a.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script>
 <?php wp_footer(); ?> 
 </body>
