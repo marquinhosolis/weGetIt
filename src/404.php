491b329d2359a84f3c2229bc6fc351e46f9713b1
marquinhosolis <marquinhosolis@gmail.com>
2020-03-28 17:52:52 -0300

    404 page

--- src/404.php
+++ src/404.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
 <main class="page404">
-	
+	<div class="container" data-aos="fade-up">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/404-text.png" alt="">
+        <h1>sorry, this page wasn’t found</h1>
+        <a href="<?php echo site_url(); ?>" class="btn hvr-shutter-out-horizontal">go back to home</a>
+    </div>
 </main>
 
 <?php get_footer(); ?>
