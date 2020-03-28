577e55abe2b0feaba158dccad74fe25fa352b459
marquinhosolis <marquinhosolis@gmail.com>
2020-03-28 17:00:29 -0300

    Addin WP to contact Page

--- src/page-contact-us.php
+++ src/page-contact-us.php
@@ -6,14 +6,14 @@ Template Name: Contact Us Page
 
 <?php get_header(); ?>
 <main class="contactPage">
-    <div class="contactPageImage" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/contact-page-image.png);">
+    <div class="contactPageImage" style="background-image: url(<?php the_field('side_image'); ?>);" data-aos="fade-right">
         <!-- <div class="content"></div> -->
     </div>
-    <div class="contactPageText">
-        <div class="contactPageTextTitle title" data-aos="fade-down">
+    <div class="contactPageText" data-aos="fade-up">
+        <div class="contactPageTextTitle title" >
             <h3>Contact</h3>
         </div>
-        <p>Whether you have a question, comment or suggestion, we'd love to hear from you! <br>Use the form below to send us an email.</p>
+        <?php the_field('form_copy'); ?>
         <div class="contactPageForm">
             <?php echo do_shortcode('[formidable id=3]'); ?>
         </div>
