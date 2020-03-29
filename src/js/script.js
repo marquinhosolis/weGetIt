16264c051eebd4d92ef1e93d1465a4f10a1a8e67
marquinhosolis <marquinhosolis@gmail.com>
2020-03-29 16:31:38 -0300

    Where to buy page

--- src/js/script.js
+++ src/js/script.js
@@ -47,8 +47,104 @@ function slider() {
     }
 }
 
+function storeList() {
+    if ($(window).width() > 768) {
+        $(".storeList ul").css("max-height", $(window).height() - 300);
+    } else {
+        $(".storeList ul").css("max-height", $(window).height() - 120);
+    }
+
+    $(".storeList li").each(function() {
+        $(this).click(function(e) {
+            e.preventDefault();
+            if ($(window).width() < 768) {
+                $(document).scrollTop($("#mapArea").offset().top - 30);
+            }
+            var src = $(this)
+                .find(".mapLink")
+                .attr("href");
+            var iframe = $(".mapIframe").find("iframe");
+            iframe.attr("src", src);
+        });
+    });
+
+    $(".storePhone a, .storeSite a").click(function(e) {
+        e.stopPropagation();
+    });
+}
+
+function storeFilter() {
+    arrCity = [];
+    arrState = [];
+    $(".store").each(function() {
+        var city = $(this)
+            .find(".storeCity")
+            .text();
+        if (jQuery.inArray(city, arrCity) == -1) {
+            arrCity.push(city);
+            arrCity.sort();
+        }
+
+        var state = $(this)
+            .find(".storeState")
+            .text();
+        if (jQuery.inArray(state, arrState) == -1) {
+            arrState.push(state);
+            arrState.sort();
+        }
+    });
+
+    for (var i = 0; i < arrCity.length; i++) {
+        $("#storeCity").append(
+            $("<option>", {
+                value: arrCity[i],
+                text: arrCity[i]
+            })
+        );
+    }
+
+    for (var i = 0; i < arrState.length; i++) {
+        $("#storeState").append(
+            $("<option>", {
+                value: arrState[i],
+                text: arrState[i]
+            })
+        );
+    }
+
+    var iframe1 = $(".storeList li")
+        .first()
+        .find("a")
+        .attr("href");
+    $(".mapIframe iframe").attr("src", iframe1);
+
+    $(".storeFilter select").change(function() {
+        var selected = $(this).val();
+        var select = $(this).attr("id");
+
+        if (selected != "") {
+            $(".store").hide();
+
+            $(".store").each(function() {
+                var needle = $(this)
+                    .find("." + select)
+                    .text();
+                console.log("needle" + needle);
+                console.log("selected: " + selected);
+                if (selected === needle) {
+                    $(this).show();
+                }
+            });
+        } else {
+            $(".store").show();
+        }
+    });
+}
+
 $(document).ready(function() {
     menuActive();
     AOS.init();
     slider();
+    storeList();
+    storeFilter();
 });
---
+++ src/page-where-to-buy.php
@@ -0,0 +1,60 @@
+<?php
+/*
+Template Name: Where to Buy Page
+*/
+?>
+
+<?php get_header(); ?>
+<main class="whereToBuyPage" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/wp/where-to-buy-bg.png); ">
+    <div class="container">
+        <div class="whereToBuyPageTitle">
+            <h1><span> Looking for</span><br>WeGetIt products near you?</h1>
+            <p>Enter your location below to find WeGetIt at a store close to you. <br> Please note that not all locations will carry the full range of WeGetIt products.</p>
+        </div>
+        <section class="storeFilters">
+            <div class="storeFilter">
+                <label><i class="fa fa-angle-down" aria-hidden="true"></i></label>
+                <select name="city" id="storeCity">
+                    <option value="" selected>Select your city</option>
+                </select>
+            </div>
+            <div class="storeFilter">
+                <label><i class="fa fa-angle-down" aria-hidden="true"></i></label>
+                <select name="state" id="storeState">
+                    <option value="" selected>Select your state</option>
+                </select>
+            </div>
+        </section>
+        <section class="storeResults">
+            <div class="storeList">
+                <ul>
+                    <?php
+                        if( have_rows('store_list') ):
+                            while ( have_rows('store_list') ) : the_row(); ?>
+                                <li class="store">
+                                    <a href="<?php the_sub_field('store_maps_address'); ?>" target="_blank" class="mapLink">
+                                        <div class="storeName"><?php the_sub_field('store_name'); ?></div>
+                                        <div class="storeAddress"><?php the_sub_field('store_address'); ?></div>
+                                        <div class="storeCity"><?php the_sub_field('storecity'); ?></div>
+                                        <div class="storeState"><?php the_sub_field('store_state'); ?></div>
+                                        <div class="storeZip"><?php the_sub_field('store_zip'); ?></div>
+                                        <div class="storePhone">
+                                            <a href="tel:<?php the_sub_field('store_phone'); ?>"><?php the_sub_field('store_phone'); ?></a>
+                                        </div>
+                                        <div class="storeSite">
+                                            <a href="<?php the_sub_field('store_site'); ?>" target="_blank"><?php the_sub_field('store_site'); ?></a>
+                                        </div>
+                                    </a>
+                                </li> 
+                            <?php endwhile;
+                        endif;
+                    ?>
+                </ul>
+            </div>
+            <div class="mapIframe" id="mapArea">
+                <iframe src="" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            </div>
+        </section>
+    </div>
+</main>	
+<?php get_footer(); ?>
